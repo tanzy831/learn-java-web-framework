d459305cd2026aafc3ea532993d54f5db4dbe63b
tanzy831 <tanzy831@gmail.com>
2015-12-06 17:32:17 +0800

    finish ClassHelper and ReflectionUtil

--- smart-framework/src/main/java/com/michael/framework/helper/ClassHelper.java
+++ smart-framework/src/main/java/com/michael/framework/helper/ClassHelper.java
@@ -1,8 +1,11 @@
 package com.michael.framework.helper;
 
-        import com.michael.framework.util.ClassUtil;
+import com.michael.framework.annotation.Controller;
+import com.michael.framework.annotation.Service;
+import com.michael.framework.util.ClassUtil;
 
-        import java.util.Set;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by tanzy on 12/4/2015.
@@ -16,5 +19,53 @@ public final class ClassHelper {
     static {
         String basePackage = ConfigHelper.getAppBasePackage();
         CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * attain all class under the base package
+     * @return
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    /**
+     * return set of classes with annotation "Service"
+     * @return
+     */
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for(Class<?> cls: CLASS_SET) {
+            if(cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * return set of classes with annotation "Controller"
+     * @return
+     */
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for(Class<?> cls: CLASS_SET) {
+            if(cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * return set of bean classes,
+     * these classes have annotation "Service" or "Controller"
+     * @return
+     */
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<Class<?>>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
     }
 }
