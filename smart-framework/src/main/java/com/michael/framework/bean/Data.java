57888eab514bc52fe910609bfbc288bdadaa5bc8
tanzy831 <tanzy831@gmail.com>
2016-01-27 22:19:29 +0800

    add Data bean

---
+++ smart-framework/src/main/java/com/michael/framework/bean/Data.java
@@ -0,0 +1,19 @@
+package com.michael.framework.bean;
+
+/**
+ * Created by tanzy on 1/27/2016.
+ */
+public class Data {
+    /**
+     * data model
+     */
+    private Object model;
+
+    public Data(Object model) {
+        this.model = model;
+    }
+
+    public Object getModel() {
+        return model;
+    }
+}
